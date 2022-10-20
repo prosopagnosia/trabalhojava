1b47bad224c38bcf106ace0140e6b411900cd724
prosopagnosia <112995731+prosopagnosia@users.noreply.github.com>
2022-10-19 21:57:36 -0300

    Update ContaEspecial.java

--- ContaEspecial.java
+++ ContaEspecial.java
@@ -1,29 +1,34 @@
 package trabalho;
 
 public class ContaEspecial extends Conta{
-		
+
 	private double limite;
 
-	
+
 	public ContaEspecial(String nome, String cpf, int numero, double valorConta, double limite) {
 		super(nome, cpf, numero, valorConta);
 		this.limite = limite;
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public double getLimite() {
+		return limite;
+	}
+
+
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+
+	public double saldo (){
+		return (this.getValorConta() + this.getLimite());
+
+
+	}
+
+
+
+
+
+
 }
